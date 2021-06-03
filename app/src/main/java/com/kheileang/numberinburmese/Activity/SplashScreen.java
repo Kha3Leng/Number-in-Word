ae8e7d7659f6fd6307a3a508e2ad7441b1f76f9b
Thura <saikhae8@gmail.com>
2021-06-03 15:39:13 +0630

    add Splash Activity

---
+++ app/src/main/java/com/kheileang/numberinburmese/Activity/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.kheileang.numberinburmese.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kheileang.numberinburmese.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
